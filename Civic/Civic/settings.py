b2422b2b08c8edbdf5fb7b1601a902caabfc81d4
User <test@example.com>
2026-04-05 15:28:28 +0530

    Initial commit

--- Civic/Civic/settings.py
+++ Civic/Civic/settings.py
@@ -288,10 +288,12 @@ GOOGLE_CLIENT_ID = _clean_google_client_id_env(
 # ========================
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.sendgrid.net')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS = True
-EMAIL_TIMEOUT = 15
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER or 'webmaster@localhost'
+
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')   # apikey
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
